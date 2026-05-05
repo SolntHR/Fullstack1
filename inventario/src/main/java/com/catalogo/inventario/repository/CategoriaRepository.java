08f230ff15ee5ec4241913118c4c15adc4fdc583
SolntHR <solangehernandz@outlook.com>
2026-05-05 14:59:38 -0400

    Repository MAYU
    MAYUSCULAS

--- inventario/src/main/java/com/catalogo/inventario/repository/CategoriaRepository.java
+++ inventario/src/main/java/com/catalogo/inventario/repository/CategoriaRepository.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
 
-    Optional<Categoria> findById_categoria(Integer id_categoria);
+    Optional<Categoria> findById_Categoria(Integer id_categoria);
 
-    Optional<Categoria> findByNombre_categoria(String nombre_categoria);
+    Optional<Categoria> findByNombre_CategoriaIgnoreCase(String nombre_categoria);
     
 }
