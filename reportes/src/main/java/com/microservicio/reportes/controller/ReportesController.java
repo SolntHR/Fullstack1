04c8763910dec5505ce3fb1898980db89a245888
jeoropeza-sudo <je.oropeza@duocuc.cl>
2026-05-25 20:32:09 -0400

    le quite los comentarios

--- reportes/src/main/java/com/microservicio/reportes/controller/ReportesController.java
+++ reportes/src/main/java/com/microservicio/reportes/controller/ReportesController.java
@@ -1,5 +1,14 @@
 package com.microservicio.reportes.controller;
 
-public class ReportesController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.microservicio.reportes.model.Reportes;
+
+@Repository
+public interface ReportesController extends JpaRepository<Reportes, Integer> {
+
+    @Autowired
 
 }
--- reportes/src/main/java/com/microservicio/reportes/model/Reportes.java
+++ reportes/src/main/java/com/microservicio/reportes/model/Reportes.java
@@ -1,5 +1,37 @@
 package com.microservicio.reportes.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name="Reportes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor 
+
 public class Reportes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idReporte;
+
+    @NotBlank(message = "El nombre del reporte no puede estar vacio")
+    @Column(name = "nombreReporte", length = 20, nullable = false, unique = true)
+    private String nombreReporte;
+
+    @NotBlank(message = "El tipo del reporte no puede estar vacio")
+    @Column(name = "tipoReporte", length = 20, nullable = false)
+    private String tipoReporte;
+
+    @NotNull(message = "La fecha de inicio del reporte no puede ser nula")
+    @Column(name = "fechaInicio", nullable = false)
+    private String fechaInicio; 
+
+    @NotNull(message = "La fecha de fin del reporte no puede ser nula")
+    @Column(name = "fechaFin", nullable = false)
+    private String fechaFin;
 
 }
