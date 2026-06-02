436d85ca1986e593cf8ac3a76251d56cfb889e64
SolntHR <solangehernandz@outlook.com>
2026-06-01 22:54:13 -0400

    Space

--- resena/src/main/java/com/resena/resena/repository/ResenaRepository.java
+++ resena/src/main/java/com/resena/resena/repository/ResenaRepository.java
@@ -7,8 +7,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface ResenaRepository extends JpaRepository<Resena,Integer>{
     Optional<Resena> findByIdResena(Integer idResena);
