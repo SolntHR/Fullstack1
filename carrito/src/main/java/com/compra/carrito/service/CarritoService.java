8f0b7b69cd6f75280c4d8c82c6b7992fd3a55c89
jeoropeza-sudo <je.oropeza@duocuc.cl>
2026-06-01 22:01:33 -0400

    conexion con cupones

--- carrito/src/main/java/com/compra/carrito/service/CarritoService.java
+++ carrito/src/main/java/com/compra/carrito/service/CarritoService.java
@@ -1,9 +1,12 @@
 package com.compra.carrito.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.compra.carrito.cliente.InventarioCliente;
 import com.compra.carrito.dto.CuponDTO;
@@ -74,22 +77,33 @@ public class CarritoService {
             .orElseThrow(() -> new RuntimeException("Carrito no encontrado"));
     }
 
-    public Integer aplicarCupon(
-            Integer total,
-            String codigoPromocional) {
+    public Integer aplicarCupon(Integer total, String codigoPromocional) {
+        RestTemplate restTemplate = new RestTemplate();
+        
+        String url = "http://localhost:8088/promociones/buscar-codigo/" + codigoPromocional;
 
-        CuponDTO cupon =
-                promocion.obtenerCupon(codigoPromocional);
+        try {
+            CuponDTO cupon = restTemplate.getForObject(url, CuponDTO.class);
 
-        if(cupon != null &&
-           cupon.getActivo()) {
+            if (cupon != null) {
+                LocalDate hoy = LocalDate.now();
+                BigDecimal totalBD = BigDecimal.valueOf(total);
 
-            Integer descuento =
-                    total * cupon.getDescuento() / 100;
+                if (hoy.isBefore(cupon.getFechaInicio()) || hoy.isAfter(cupon.getFechaFin())) {
+                    throw new RuntimeException("El cupón no está vigente hoy");
+                }
 
-            return total - descuento;
+                if (totalBD.compareTo(cupon.getMontoMinimo()) < 0) {
+                    throw new RuntimeException("El total debe ser al menos $" + cupon.getMontoMinimo());
+                }
+
+                BigDecimal descuento = totalBD.multiply(cupon.getDescuento()).divide(BigDecimal.valueOf(100));
+                return total - descuento.intValue();
+            }
+        } catch (Exception e) {
+            System.out.println("Error al conectar con promociones: " + e.getMessage());
         }
-
         return total;
     }
+
 }
