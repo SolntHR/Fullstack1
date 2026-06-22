e0b25f98a189e4d052de2e64453400c91f16348d
SolntHR <solangehernandz@outlook.com>
2026-06-22 16:30:06 -0400

    Ticket
    No funcionaba, ahora si xd - Faltaba una dependency management que no tenia.

--- ticket/ticket/src/main/java/com/soporte/ticket/controller/TicketController.java
+++ ticket/ticket/src/main/java/com/soporte/ticket/controller/TicketController.java
@@ -28,8 +28,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 
 
 @RestController
